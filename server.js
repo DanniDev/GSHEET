4f17e85ef52e04cb346dee918c9831f6acacec8d
Danny <coderdanny2018@gmail.com>
2022-12-02 19:30:44 +0000

    eighteen  commit

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.post('/saved-list', (req, res) => {
 	console.log('received request from softr');
 	const body = req.body;
 	console.log(body);
-	res.send('success');
+	res.status(200).json({ success: 'success' });
 });
 
 app.listen(PORT, () =>
