6fa535f33c91a45bccfc3600416c5feb11e87956
Daniel Agyei <dr4lyf@gmail.com>
2023-06-27 02:51:47 +0000

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(morgan('dev'));
 
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.port || 5000;
 
 app.get('/', function (req, res) {
 	return res.send('Close connected to server!');
